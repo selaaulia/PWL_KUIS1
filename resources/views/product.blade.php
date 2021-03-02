f0f8ba2beb5044fc15b400241b6b3b5d073fcdf4
selaaulia <selaaulia1307@gmail.com>
2021-03-02 18:59:43 +0700

    Praktikum2

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -9,202 +9,159 @@
 @endsection
 
 @section('content')
-        <!-- Page Content -->
-    <!-- Banner Starts Here -->
-    <div class="banner header-text">
-        <div class="owl-banner owl-carousel">
-          <div class="banner-item-01">
+           <!-- Page Content -->
+    <div class="page-heading products-heading header-text">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
             <div class="text-content">
-              <h4>Best Offer</h4>
-              <h2>New Arrivals On Sale</h2>
-            </div>
-          </div>
-          <div class="banner-item-02">
-            <div class="text-content">
-              <h4>Flash Deals</h4>
-              <h2>Get your best products</h2>
-            </div>
-          </div>
-          <div class="banner-item-03">
-            <div class="text-content">
-              <h4>Last Minute</h4>
-              <h2>Grab last minute deals</h2>
+              <h4>new arrivals</h4>
+              <h2>sixteen products</h2>
             </div>
           </div>
         </div>
       </div>
-      <!-- Banner Ends Here -->
-  
-      <div class="latest-products">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-12">
-              <div class="section-heading">
-                <h2>Latest Products</h2>
-                <a href="products.html">view all products <i class="fa fa-angle-right"></i></a>
-              </div>
+    </div>
+
+    
+    <div class="products">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="filters">
+              <ul>
+                  <li class="active" data-filter="*">All Products</li>
+                  <li data-filter=".des">Featured</li>
+                  <li data-filter=".dev">Flash Deals</li>
+                  <li data-filter=".gra">Last Minute</li>
+              </ul>
             </div>
-            <div class="col-md-4">
-              <div class="product-item">
-                <a href="#"><img src="assets/images/product_01.jpg" alt=""></a>
-                <div class="down-content">
-                  <a href="#"><h4>Tittle goes here</h4></a>
-                  <h6>$25.75</h6>
-                  <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
-                  <ul class="stars">
-                    <li><i class="fa fa-star"></i></li>
-                    <li><i class="fa fa-star"></i></li>
-                    <li><i class="fa fa-star"></i></li>
-                    <li><i class="fa fa-star"></i></li>
-                    <li><i class="fa fa-star"></i></li>
-                  </ul>
-                  <span>Reviews (24)</span>
+          </div>
+          <div class="col-md-12">
+            <div class="filters-content">
+                <div class="row grid">
+                    <div class="col-lg-4 col-md-4 all des">
+                      <div class="product-item">
+                        <a href="#"><img src="assets/images/product_01.jpg" alt=""></a>
+                        <div class="down-content">
+                          <a href="#"><h4>Tittle goes here</h4></a>
+                          <h6>$18.25</h6>
+                          <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
+                          <ul class="stars">
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                          </ul>
+                          <span>Reviews (12)</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 all dev">
+                      <div class="product-item">
+                        <a href="#"><img src="assets/images/product_02.jpg" alt=""></a>
+                        <div class="down-content">
+                          <a href="#"><h4>Tittle goes here</h4></a>
+                          <h6>$16.75</h6>
+                          <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
+                          <ul class="stars">
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                          </ul>
+                          <span>Reviews (24)</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 all gra">
+                      <div class="product-item">
+                        <a href="#"><img src="assets/images/product_03.jpg" alt=""></a>
+                        <div class="down-content">
+                          <a href="#"><h4>Tittle goes here</h4></a>
+                          <h6>$32.50</h6>
+                          <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
+                          <ul class="stars">
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                          </ul>
+                          <span>Reviews (36)</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 all gra">
+                      <div class="product-item">
+                        <a href="#"><img src="assets/images/product_04.jpg" alt=""></a>
+                        <div class="down-content">
+                          <a href="#"><h4>Tittle goes here</h4></a>
+                          <h6>$24.60</h6>
+                          <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
+                          <ul class="stars">
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                          </ul>
+                          <span>Reviews (48)</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 all dev">
+                      <div class="product-item">
+                        <a href="#"><img src="assets/images/product_05.jpg" alt=""></a>
+                        <div class="down-content">
+                          <a href="#"><h4>Tittle goes here</h4></a>
+                          <h6>$18.75</h6>
+                          <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
+                          <ul class="stars">
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                          </ul>
+                          <span>Reviews (60)</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 all des">
+                      <div class="product-item">
+                        <a href="#"><img src="assets/images/product_06.jpg" alt=""></a>
+                        <div class="down-content">
+                          <a href="#"><h4>Tittle goes here</h4></a>
+                          <h6>$12.50</h6>
+                          <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
+                          <ul class="stars">
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                          </ul>
+                          <span>Reviews (72)</span>
+                        </div>
+                      </div>
+                    </div>
                 </div>
-              </div>
             </div>
-            <div class="col-md-4">
-              <div class="product-item">
-                <a href="#"><img src="assets/images/product_02.jpg" alt=""></a>
-                <div class="down-content">
-                  <a href="#"><h4>Tittle goes here</h4></a>
-                  <h6>$30.25</h6>
-                  <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
-                  <ul class="stars">
-                    <li><i class="fa fa-star"></i></li>
-                    <li><i class="fa fa-star"></i></li>
-                    <li><i class="fa fa-star"></i></li>
-                    <li><i class="fa fa-star"></i></li>
-                    <li><i class="fa fa-star"></i></li>
-                  </ul>
-                  <span>Reviews (21)</span>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="product-item">
-                <a href="#"><img src="assets/images/product_03.jpg" alt=""></a>
-                <div class="down-content">
-                  <a href="#"><h4>Tittle goes here</h4></a>
-                  <h6>$20.45</h6>
-                  <p>Sixteen Clothing is free CSS template provided by TemplateMo.</p>
-                  <ul class="stars">
-                    <li><i class="fa fa-star"></i></li>
-                    <li><i class="fa fa-star"></i></li>
-                    <li><i class="fa fa-star"></i></li>
-                    <li><i class="fa fa-star"></i></li>
-                    <li><i class="fa fa-star"></i></li>
-                  </ul>
-                  <span>Reviews (36)</span>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="product-item">
-                <a href="#"><img src="assets/images/product_04.jpg" alt=""></a>
-                <div class="down-content">
-                  <a href="#"><h4>Tittle goes here</h4></a>
-                  <h6>$15.25</h6>
-                  <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
-                  <ul class="stars">
-                    <li><i class="fa fa-star"></i></li>
-                    <li><i class="fa fa-star"></i></li>
-                    <li><i class="fa fa-star"></i></li>
-                    <li><i class="fa fa-star"></i></li>
-                    <li><i class="fa fa-star"></i></li>
-                  </ul>
-                  <span>Reviews (48)</span>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="product-item">
-                <a href="#"><img src="assets/images/product_05.jpg" alt=""></a>
-                <div class="down-content">
-                  <a href="#"><h4>Tittle goes here</h4></a>
-                  <h6>$12.50</h6>
-                  <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
-                  <ul class="stars">
-                    <li><i class="fa fa-star"></i></li>
-                    <li><i class="fa fa-star"></i></li>
-                    <li><i class="fa fa-star"></i></li>
-                    <li><i class="fa fa-star"></i></li>
-                    <li><i class="fa fa-star"></i></li>
-                  </ul>
-                  <span>Reviews (16)</span>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="product-item">
-                <a href="#"><img src="assets/images/product_06.jpg" alt=""></a>
-                <div class="down-content">
-                  <a href="#"><h4>Tittle goes here</h4></a>
-                  <h6>$22.50</h6>
-                  <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
-                  <ul class="stars">
-                    <li><i class="fa fa-star"></i></li>
-                    <li><i class="fa fa-star"></i></li>
-                    <li><i class="fa fa-star"></i></li>
-                    <li><i class="fa fa-star"></i></li>
-                    <li><i class="fa fa-star"></i></li>
-                  </ul>
-                  <span>Reviews (32)</span>
-                </div>
-              </div>
-            </div>
+          </div>
+          <div class="col-md-12">
+            <ul class="pages">
+              <li><a href="#">1</a></li>
+              <li class="active"><a href="#">2</a></li>
+              <li><a href="#">3</a></li>
+              <li><a href="#">4</a></li>
+              <li><a href="#"><i class="fa fa-angle-double-right"></i></a></li>
+            </ul>
           </div>
         </div>
       </div>
-  
-      <div class="best-features">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-12">
-              <div class="section-heading">
-                <h2>About Sixteen Clothing</h2>
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div class="left-content">
-                <h4>Looking for the best products?</h4>
-                <p><a rel="nofollow" href="https://templatemo.com/tm-546-sixteen-clothing" target="_parent">This template</a> is free to use for your business websites. However, you have no permission to redistribute the downloadable ZIP file on any template collection website. <a rel="nofollow" href="https://templatemo.com/contact">Contact us</a> for more info.</p>
-                <ul class="featured-list">
-                  <li><a href="#">Lorem ipsum dolor sit amet</a></li>
-                  <li><a href="#">Consectetur an adipisicing elit</a></li>
-                  <li><a href="#">It aquecorporis nulla aspernatur</a></li>
-                  <li><a href="#">Corporis, omnis doloremque</a></li>
-                  <li><a href="#">Non cum id reprehenderit</a></li>
-                </ul>
-                <a href="/about" class="filled-button">Read More</a>
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div class="right-image">
-                <img src="assets/images/feature-image.jpg" alt="">
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-  
-  
-      <div class="call-to-action">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-12">
-              <div class="inner-content">
-                <div class="row">
-                  <div class="col-md-8">
-                    <h4>Creative &amp; Unique <em>Sixteen</em> Products</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque corporis amet elite author nulla.</p>
-                  </div>
-                  <div class="col-md-4">
-                    <a href="#" class="filled-button">Purchase Now</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    </div>
+
 @endsection
